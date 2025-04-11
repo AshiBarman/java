c9877dbafcafddb698c2d37a5b964641a712cad0
AshiBarman <ashibarman567@gmail.com>
2025-04-10 23:03:54 -0700

    add

---
+++ Days.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Days {
+    public static void main(String [] args){
+       Scanner in=new Scanner (System.in);
+       System.out.println("Enter Day (1-7)");
+       int a=in.nextInt();
+       switch (a){
+        case 1:
+        System.out.println("Monday"));
+        break;
+        case 2:
+        System.out.println("Tuesday");
+        break;
+        case 3:
+        System.out.println("Wednesday");
+        break;
+        case 4:
+        System.out.println("Thursday");
+        break;
+        case 5:
+        System.out.println("Friday");
+        break;
+        case 6:
+        System.out.println("Saturday");
+        break;
+        case 7:
+        System.out.println("Sunday");
+        break;
+        case 8:
+        System.out.println("Invalid");
+        break;
+              }
+            }
+        }
