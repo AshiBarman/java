4484eea64197251768f036f84bdca0a66eada5cb
AshiBarman <ashibarman567@gmail.com>
2025-06-03 21:44:32 -0700

    RunnableQ

--- RunnableQ.java
+++ RunnableQ.java
@@ -8,6 +8,15 @@ public class RunnableQ {
         MyRunnable obj = new MyRunnable();
         Thread t1 = new Thread(obj);
         t1.start();
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block   t1.join is used to block the main thread.
+            e.printStackTrace();
+        }
+        for(int i =1;i<=5;i++){
+            System.out.println(i);
+        }
         System.out.println("Main thread is running");
     }
 }
