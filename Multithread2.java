fa6754333761ecbcb05abc5171844e2ebc6a81d3
AshiBarman <ashibarman567@gmail.com>
2025-06-05 00:46:58 -0700

    Multithread2

---
+++ Multithread2.java
@@ -0,0 +1,20 @@
+class Multithread2{
+    public static void main(String[] args) {
+        System.out.println("idhar bilkul mat dekhoo");
+        Thread t1 = new Thread(()->{
+        System.out.println("hiiiiii");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println("bye");
+        });
+        Thread t2 = new Thread(()->{
+        System.out.println("hlooo");
+        });
+        t1.start();
+        t2.start();
+    }
+}
